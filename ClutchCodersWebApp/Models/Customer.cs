1d7b765b2e57ecd16917e760110fe95f2739b7db
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-11-25 18:59:57 +0000

    half did customer

--- ClutchCodersWebApp/Models/Customer.cs
+++ ClutchCodersWebApp/Models/Customer.cs
@@ -2,5 +2,17 @@
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string PhoneNo { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsEmployee { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
     }
 }
