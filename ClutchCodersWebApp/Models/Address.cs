c16f30b074c7e2da9413bb0b9ea97af739811fea
4drvim61 <4drivm61@solent.ac.uk>
2021-11-25 21:43:01 +0000

    address class

---
+++ ClutchCodersWebApp/Models/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClutchCodersWebApp.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string FirstLine { get; set; }
+        [Required, StringLength(50)]
+        public string SecondLine { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [Required, StringLength(10)]
+        public string Postcode { get; set; }
+        //
+        // PERSONID MAYBE REMOVE
+        //
+        [Required]
+        public int PersonId { get; set; }
+
+    }
+}
