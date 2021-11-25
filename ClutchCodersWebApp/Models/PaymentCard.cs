3dec0aec60eb67b585c775ba240c27bb18cd6552
4drvim61 <4drivm61@solent.ac.uk>
2021-11-25 21:48:59 +0000

    Payment card class

---
+++ ClutchCodersWebApp/Models/PaymentCard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClutchCodersWebApp.Models
+{
+    public class PaymentCard
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(30)]
+        public string NameOnCard { get; set; }
+        [Required]
+        public int ExpiryDate { get; set; }
+        [Required]
+        public string SecurityCode { get; set; }
+        //
+        // PERSONID MAYBE REMOVE
+        //
+        [Required]
+        public int PersonId { get; set; }
+
+    }
+}
