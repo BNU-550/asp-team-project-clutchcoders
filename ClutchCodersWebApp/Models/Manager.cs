2ed75fbaf6b3c6876fdd1a899ae447a56f55bca1
4drvim61 <4drivm61@solent.ac.uk>
2021-11-25 18:59:59 +0000

    manager class

---
+++ ClutchCodersWebApp/Models/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClutchCodersWebApp.Models
+{
+    public class Manager
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        [StringLength(10)]
+        public string PhoneNo { get; set; }
+
+        public string Email { get; set; }
+
+        public Boolean IsEmployee { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
