ab13985e602d20f86e60b6f6c2dcbe6b4397fc3c
4drvim61 <4drivm61@solent.ac.uk>
2021-11-25 19:50:49 +0000

    customer class updated

--- ClutchCodersWebApp/Models/Customer.cs
+++ ClutchCodersWebApp/Models/Customer.cs
@@ -1,17 +1,22 @@
-﻿namespace ClutchCodersWebApp.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClutchCodersWebApp.Models
 {
     public class Customer
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        
+
+        [Required, StringLength(25)]
         public string PhoneNo { get; set; }
-
+        [Required, StringLength(30)]
         public string Email { get; set; }
-
-        public bool IsEmployee { get; set; }
-
+        [Required]
+        public Boolean IsEmployee { get; set; }
+        [Required, StringLength(20)]
         public string FirstName { get; set; }
+        [Required, StringLength(20)]
         public string LastName { get; set; }
 
     }
