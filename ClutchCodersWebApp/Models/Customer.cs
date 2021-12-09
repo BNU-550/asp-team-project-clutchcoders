cea415e368ad971030a070382e4bb7122548ae6d
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-12-09 12:59:35 +0000

    fixed readability

--- ClutchCodersWebApp/Models/Customer.cs
+++ ClutchCodersWebApp/Models/Customer.cs
@@ -20,8 +20,12 @@ namespace ClutchCodersWebApp.Models
 
         [Required]
         public Boolean IsEmployee { get; set; }
+
+
         [Required, StringLength(20)]
         public string FirstName { get; set; }
+
+
         [Required, StringLength(20, MinimumLength = 2)]
         public string LastName { get; set; }
 
