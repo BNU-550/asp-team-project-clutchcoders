bcc251af5a268a136dedf5208619991289027b74
4drvim61 <4drivm61@solent.ac.uk>
2021-12-09 12:56:49 +0000

    min length 2

--- ClutchCodersWebApp/Models/Customer.cs
+++ ClutchCodersWebApp/Models/Customer.cs
@@ -15,9 +15,9 @@ namespace ClutchCodersWebApp.Models
         public string Email { get; set; }
         [Required]
         public Boolean IsEmployee { get; set; }
-        [Required, StringLength(20)]
+        [Required, StringLength(20, MinimumLength = 2)]
         public string FirstName { get; set; }
-        [Required, StringLength(20)]
+        [Required, StringLength(20, MinimumLength = 2)]
         public string LastName { get; set; }
 
     }
