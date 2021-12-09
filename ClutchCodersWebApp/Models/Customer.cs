00dbd34406e26f8016095a3f2bda45f83c333a83
4drvim61 <4drivm61@solent.ac.uk>
2021-12-09 13:09:06 +0000

    display name customer

--- ClutchCodersWebApp/Models/Customer.cs
+++ ClutchCodersWebApp/Models/Customer.cs
@@ -23,10 +23,12 @@ namespace ClutchCodersWebApp.Models
 
 
         [Required, StringLength(20)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
 
         [Required, StringLength(20, MinimumLength = 2)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
     }
