c30578c94f3815231c594e24733b94412bb7ced1
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-12-10 21:07:18 +0000

    messing with index page

---
+++ ClutchCodersWebApp/Pages/Search.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClutchCodersWebApp.Pages
+{
+    public class SearchModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public SearchModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
