9d39ddf775cc1aeab1b45056fe0c028f99251b18
4drvim61 <4drivm61@solent.ac.uk>
2021-12-11 18:10:27 +0000

    payment cards added

--- ClutchCodersWebApp/Data/DbInitialiser.cs
+++ ClutchCodersWebApp/Data/DbInitialiser.cs
@@ -19,12 +19,107 @@ namespace ClutchCodersWebApp.Data
         //TODO: Add 10 people and 2 staff
         //TODO: Add 12 addresses
         //TODO: Add call initialise method in the program class
+        private static void AddPaymentCards(ApplicationDbContext context)
+        {
+            if (context.PaymentCards.Any())
+            {
+                return;
+            }
+
+            var cards = new PaymentCard[]
+            {
+                new PaymentCard()
+                {
+                    CardNumber = "8361 4019 5729 9172",
+                    NameOnCard = "Dave Smith",
+                    ExpiryYear = 2023,
+                    ExpiryMonth = 8,
+                    SecurityCode = "927"
+                },
+                new PaymentCard()
+                {
+                    CardNumber = "1857 2740 7592 7281",
+                    NameOnCard = "John Walker",
+                    ExpiryYear = 2026,
+                    ExpiryMonth = 2,
+                    SecurityCode = "891"
+                },
+                new PaymentCard()
+                {
+                    CardNumber = "3718 0572 7593 1074",
+                    NameOnCard = "Graham Fanta",
+                    ExpiryYear = 2024,
+                    ExpiryMonth = 5,
+                    SecurityCode = "294"
+                },
+                new PaymentCard()
+                {
+                    CardNumber = "2974 4728 0183 4628",
+                    NameOnCard = "Sharon Case",
+                    ExpiryYear = 2028,
+                    ExpiryMonth = 9,
+                    SecurityCode = "919"
+                },
+                new PaymentCard()
+                {
+                    CardNumber = "1847 5973 0317 5628",
+                    NameOnCard = "Anna Becker",
+                    ExpiryYear = 2021,
+                    ExpiryMonth = 10,
+                    SecurityCode = "137"
+                },
+                new PaymentCard()
+                {
+                    CardNumber = "1749 5628 6183 0572",
+                    NameOnCard = "Jake Price",
+                    ExpiryYear = 2027,
+                    ExpiryMonth = 11,
+                    SecurityCode = "482"
+                },
+                new PaymentCard()
+                {
+                    CardNumber = "1847 9573 5729 5661",
+                    NameOnCard = "Vinny Thread",
+                    ExpiryYear = 2028,
+                    ExpiryMonth = 5,
+                    SecurityCode = "007"
+                },
+                new PaymentCard()
+                {
+                    CardNumber = "4579 4781 1208 8753",
+                    NameOnCard = "Matt Driver",
+                    ExpiryYear = 2027,
+                    ExpiryMonth = 9,
+                    SecurityCode = "217"
+                },
+                new PaymentCard()
+                {
+                    CardNumber = "1794 5827 9274 0297",
+                    NameOnCard = "Kate Turton",
+                    ExpiryYear = 2022,
+                    ExpiryMonth = 12,
+                    SecurityCode = "136"
+                },
+                new PaymentCard()
+                {
+                    CardNumber = "1937 7592 5729 9172",
+                    NameOnCard = "Sarah Walker",
+                    ExpiryYear = 2028,
+                    ExpiryMonth = 9,
+                    SecurityCode = "274"
+                },
+            };
+            foreach (PaymentCard a in cards)
+            {
+                context.PaymentCards.Add(a);
+            }
+        }
 
         private static void AddAddresses(ApplicationDbContext context)
         {
             if (context.Addresses.Any())
             {
-                return;   // DB has been seeded
+                return;
             }
 
             var addresses = new Address[]
@@ -38,7 +133,6 @@ namespace ClutchCodersWebApp.Data
                 {
                     HouseNumber = "2",
                     Postcode = "FJ4 8AS"
-                    //TODO: Add 10 addresses
                 },
                 new Address()
                 {
@@ -79,6 +173,16 @@ namespace ClutchCodersWebApp.Data
                 {
                     HouseNumber = "10",
                     Postcode = "BE3 9NA"
+                },
+                new Address()
+                {
+                    HouseNumber = "11",
+                    Postcode = "FU7 5IT"
+                },
+                new Address()
+                {
+                    HouseNumber = "12",
+                    Postcode = "JD2 7GJ"
                 }
             };
 
