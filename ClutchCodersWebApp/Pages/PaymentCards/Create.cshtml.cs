106524b11673b35e7c26d64be8f41e130c597bb6
KateTurton <rinekate@hotmail.com>
2021-12-30 17:31:20 +0000

    Seeded data to people, product and payment Added PK to staff AspNetUsers and AspUserRole Registered One staff with logging details Created and added CRUD pages to address, payment cards and people.
    Made changes to footer in staff layout
    Added txt notes to project

---
+++ ClutchCodersWebApp/Pages/PaymentCards/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ClutchCodersWebApp.Data;
+using ClutchCodersWebApp.Models;
+
+namespace ClutchCodersWebApp.Pages.PaymentCards
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ClutchCodersWebApp.Data.ApplicationDbContext _context;
+
+        public CreateModel(ClutchCodersWebApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public PaymentCard PaymentCard { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.PaymentCards.Add(PaymentCard);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ClutchCodersWebApp/Pages/People/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ClutchCodersWebApp.Data;
+using ClutchCodersWebApp.Models;
+
+namespace ClutchCodersWebApp.Pages.People
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ClutchCodersWebApp.Data.ApplicationDbContext _context;
+
+        public IndexModel(ClutchCodersWebApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Person = await _context.People
+                .Include(p => p.Address)
+                .Include(p => p.PaymentCard).ToListAsync();
+        }
+    }
+}
