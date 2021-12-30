8c1f4f0afc3302ee9e7040ff0391e12900cbb3dd
4drvim61 <4drivm61@solent.ac.uk>
2021-12-30 22:22:35 +0000

    order class made

---
+++ ClutchCodersWebApp/Models/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClutchCodersWebApp.Models
+{
+    public class Order
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name that is 1-30 characters long")]
+        [StringLength(30)]
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [StringLength(25)]
+        public string PhoneNo { get; set; }
+
+        [Required(ErrorMessage = "Please enter your post code")]
+        [StringLength(10)]
+        public string Postcode { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Delivery Date")]
+        public Nullable<DateTime> DeliveryDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Order Date")]
+        public DateTime OrderDate { get; set; }
+
+        //full name//contact number//post code//order date//delivery date
+
+    }
+}
